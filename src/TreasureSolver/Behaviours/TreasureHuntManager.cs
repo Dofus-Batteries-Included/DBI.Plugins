b7cf5225f8123dc627a9ca665fd9a38dd2c95efd
ismailbennani <ismail.lahkim.bennani@gmail.com>
2024-08-24 15:44:40 +0200

    change message when arrived

--- src/TreasureSolver/Behaviours/TreasureHuntManager.cs
+++ src/TreasureSolver/Behaviours/TreasureHuntManager.cs
@@ -224,7 +224,7 @@ public class TreasureHuntManager : MonoBehaviour
             }
             else
             {
-                stepMessage += " reached destination";
+                stepMessage += " arrived";
             }
         }
 
