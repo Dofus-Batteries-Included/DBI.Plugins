a01f077b3f52a0e699d59e718e010c2e0b062242
ismailbennani <ismail.lahkim.bennani@gmail.com>
2024-08-30 14:14:28 +0200

    register bad clues during treasure hunt

--- src/TreasureSolver/Clues/Listeners/SaveCluesOnDigAnswerEvent.cs
+++ src/TreasureSolver/Clues/Listeners/SaveCluesOnDigAnswerEvent.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Com.Ankama.Dofus.Server.Game.Protocol.Treasure.Hunt;
 using DofusBatteriesIncluded.Core;
 using DofusBatteriesIncluded.Core.Protocol;
@@ -9,16 +11,11 @@ namespace DofusBatteriesIncluded.TreasureSolver.Clues.Listeners;
 public class SaveCluesOnDigAnswerEvent : IMessageListener<TreasureHuntEvent>, IMessageListener<TreasureHuntDigAnswerEvent>
 {
     static readonly ILogger Log = DBI.Logging.Create<LocalCluesService>();
-    TreasureHuntEvent _lastHuntEvent;
+    int? _currentCheckpoint;
+    List<ClueAtPosition> _clues = [];
 
     public Task HandleAsync(TreasureHuntDigAnswerEvent message)
     {
-        if (_lastHuntEvent is null)
-        {
-            Log.LogWarning("Could not find last hunt event, cannot extract data about correct and incorrect flags.");
-            return Task.CompletedTask;
-        }
-
         if (!TreasureSolver.TryGetCluesService(out ICluesService cluesService))
         {
             Log.LogWarning("Could not get clues service, skipping Treasure Hunt Event.");
@@ -27,58 +24,80 @@ public class SaveCluesOnDigAnswerEvent : IMessageListener<TreasureHuntEvent>, IM
 
         if (message.Result is TreasureHuntDigAnswerEvent.Types.DigResult.NewHint or TreasureHuntDigAnswerEvent.Types.DigResult.Finished)
         {
-            for (int index = 0; index < _lastHuntEvent.Flags.Count; index++)
+            foreach (ClueAtPosition clue in _clues)
             {
-                TreasureHuntEvent.Types.TreasureHuntStep step = _lastHuntEvent.KnownSteps[index];
-                if (step.StepCase != TreasureHuntEvent.Types.TreasureHuntStep.StepOneofCase.FollowDirectionToPoi || step.FollowDirectionToPoi == null)
+                if (!clue.ClueId.HasValue)
                 {
                     continue;
                 }
 
-                TreasureHuntEvent.Types.TreasureHuntFlag flag = _lastHuntEvent.Flags[index];
-
-                cluesService.RegisterCluesAsync(flag.MapId, new ClueWithStatus(step.FollowDirectionToPoi.PoiLabelId, true));
+                cluesService.RegisterCluesAsync(clue.MapId, new ClueWithStatus(clue.ClueId.Value, true));
             }
         }
         else if (message.Result is TreasureHuntDigAnswerEvent.Types.DigResult.Wrong or TreasureHuntDigAnswerEvent.Types.DigResult.WrongAndYouKnowIt
                  or TreasureHuntDigAnswerEvent.Types.DigResult.Lost)
         {
-            for (int index = 0; index < _lastHuntEvent.Flags.Count; index++)
+            foreach (ClueAtPosition clue in _clues)
             {
-                TreasureHuntEvent.Types.TreasureHuntStep step = _lastHuntEvent.KnownSteps[index];
-                if (step.StepCase != TreasureHuntEvent.Types.TreasureHuntStep.StepOneofCase.FollowDirectionToPoi || step.FollowDirectionToPoi == null)
+                if (!clue.ClueId.HasValue)
                 {
                     continue;
                 }
 
-                TreasureHuntEvent.Types.TreasureHuntFlag flag = _lastHuntEvent.Flags[index];
-                if (flag.State == TreasureHuntEvent.Types.TreasureHuntFlag.Types.FlagState.Unknown)
-                {
-                    continue;
-                }
 
-                cluesService.RegisterCluesAsync(
-                    flag.MapId,
-                    new ClueWithStatus(step.FollowDirectionToPoi.PoiLabelId, flag.State == TreasureHuntEvent.Types.TreasureHuntFlag.Types.FlagState.Ok)
-                );
+                if (clue.ValidationState.HasValue)
+                {
+                    cluesService.RegisterCluesAsync(clue.MapId, new ClueWithStatus(clue.ClueId.Value, clue.ValidationState.Value));
+                }
             }
         }
 
-        _lastHuntEvent = null;
+        _currentCheckpoint = null;
 
         return Task.CompletedTask;
     }
 
     public Task HandleAsync(TreasureHuntEvent message)
     {
-        if (_lastHuntEvent != null && message.CurrentCheckPoint != _lastHuntEvent.CurrentCheckPoint)
+        if (_currentCheckpoint != null && message.CurrentCheckPoint != _currentCheckpoint.Value)
         {
             // sometimes dig events arrive after the hunt event of the next step
             // don't overwrite events of checkpoint until they are processed by the dig event handler
             return Task.CompletedTask;
         }
 
-        _lastHuntEvent = message;
+        _currentCheckpoint = message.CurrentCheckPoint;
+
+        List<ClueAtPosition> newClues = new();
+        for (int index = 0; index < message.Flags.Count; index++)
+        {
+            TreasureHuntEvent.Types.TreasureHuntStep step = message.KnownSteps.array[index];
+            TreasureHuntEvent.Types.TreasureHuntFlag flag = message.Flags.array[index];
+
+            bool? validation = flag.State switch
+            {
+
+                TreasureHuntEvent.Types.TreasureHuntFlag.Types.FlagState.Unknown => null,
+                TreasureHuntEvent.Types.TreasureHuntFlag.Types.FlagState.Ok => true,
+                TreasureHuntEvent.Types.TreasureHuntFlag.Types.FlagState.Wrong => false,
+                _ => null
+            };
+            newClues.Add(new ClueAtPosition(flag.MapId, step.FollowDirectionToPoi?.PoiLabelId, validation));
+        }
+
+        bool isDigResult = _clues.Count(c => c.ValidationState == true)
+                           != message.Flags.array.Take(message.Flags.Count).Count(f => f.State == TreasureHuntEvent.Types.TreasureHuntFlag.Types.FlagState.Ok);
+        if (isDigResult && _clues.Count > message.Flags.Count)
+        {
+            // first unknown clue was wrong
+            ClueAtPosition firstUnknownClue = _clues[message.Flags.Count];
+            newClues.Add(firstUnknownClue with { ValidationState = false });
+        }
+
+        _clues = newClues;
+
         return Task.CompletedTask;
     }
+
+    record struct ClueAtPosition(long MapId, int? ClueId, bool? ValidationState);
 }
