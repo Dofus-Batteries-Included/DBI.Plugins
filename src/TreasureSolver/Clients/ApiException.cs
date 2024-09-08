81a3065ab855a218be6f9e49e4dee8b2e2811b07
ismailbennani <ismail.lahkim.bennani@gmail.com>
2024-09-09 01:38:55 +0200

    add identity client

---
+++ src/TreasureSolver/Clients/ApiException.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DofusBatteriesIncluded.Plugins.TreasureSolver.Clients;
+
+public class ApiException : Exception
+{
+    public int StatusCode { get; private set; }
+
+    public string Response { get; }
+
+    public IReadOnlyDictionary<string, IEnumerable<string>> Headers { get; private set; }
+
+    public ApiException(string message, int statusCode, string response, IReadOnlyDictionary<string, IEnumerable<string>> headers, Exception innerException) : base(
+        message + "\n\nStatus: " + statusCode + "\nResponse: \n" + (response == null ? "(null)" : response[..(response.Length >= 512 ? 512 : response.Length)]),
+        innerException
+    )
+    {
+        StatusCode = statusCode;
+        Response = response;
+        Headers = headers;
+    }
+
+    public override string ToString() => $"HTTP Response: \n\n{Response}\n\n{base.ToString()}";
+}
+
+public class ApiException<TResult> : ApiException
+{
+    public TResult Result { get; private set; }
+
+    public ApiException(string message, int statusCode, string response, IReadOnlyDictionary<string, IEnumerable<string>> headers, TResult result, Exception innerException) : base(
+        message,
+        statusCode,
+        response,
+        headers,
+        innerException
+    )
+    {
+        Result = result;
+    }
+}
