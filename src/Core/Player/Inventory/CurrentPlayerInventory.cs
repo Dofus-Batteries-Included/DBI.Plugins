3608d3ebe9824e02985b804683c6b9caccc0b3b2
Léo M. - GrayR0ot <thisis41@protonmail.com>
2024-10-04 16:51:13 +0200

    feat: Added Player inventory to DBI CurrentCharacter

---
+++ src/Core/Player/Inventory/CurrentPlayerInventory.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Com.Ankama.Dofus.Server.Game.Protocol.Common;
+
+namespace DofusBatteriesIncluded.Plugins.Core.Player;
+
+public class CurrentPlayerInventory
+{
+
+    public List<ObjectItemInventory> Items { get; set; } = new();
+}
---
+++ src/Core/Player/Inventory/UpdateCurrentPlayerInventory.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Com.Ankama.Dofus.Server.Game.Protocol.Common;
+using Com.Ankama.Dofus.Server.Game.Protocol.Inventory;
+using DofusBatteriesIncluded.Plugins.Core.Protocol;
+using Microsoft.Extensions.Logging;
+
+namespace DofusBatteriesIncluded.Plugins.Core.Player;
+
+public class UpdateCurrentPlayerInventory : IMessageListener<InventoryContentEvent>
+{
+    static readonly ILogger Log = DBI.Logging.Create<UpdateCurrentPlayerInventory>();
+    public Task HandleAsync(InventoryContentEvent message)
+    {
+        List<ObjectItemInventory> items = new();
+        foreach (var item in message.Objects.array)
+        {
+            items.Add(item);
+        }
+
+        DBI.Player.CurrentCharacter.Inventory.Items = items;
+        Log.LogDebug("Updated current inventory with {Items} items.", items.Count);
+        return Task.CompletedTask;
+    }
+}
