ed5ed4c7245d29d6094b4d314db9b2dfbaa80c13
ismailbennani <ismail.lahkim.bennani@gmail.com>
2024-10-19 11:00:24 +0200

    plugin works

--- src/Hell/CorePlugin.cs
+++ src/Hell/CorePlugin.cs
@@ -1,15 +1,17 @@
 ﻿using BepInEx;
+using BepInEx.Unity.IL2CPP;
 using DBI.Hell.Logging;
 using Microsoft.Extensions.Logging;
 
 namespace DBI.Hell;
 
 [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
-public class CorePlugin
+[BepInProcess("Dofus.exe")]
+class CorePlugin : BasePlugin
 {
     public static readonly CoreLogging Logging = new();
 
-    protected void Load()
+    public override void Load()
     {
         ILogger logger = Logging.Create<CorePlugin>();
 
--- src/Hell/Logging/CoreLogging.cs
+++ src/Hell/Logging/CoreLogging.cs
@@ -3,7 +3,7 @@
 namespace DBI.Hell.Logging;
 
 // ReSharper disable once InconsistentNaming
-public class CoreLogging
+class CoreLogging
 {
     readonly DofusBatteriesIncludedLoggerProvider _provider = new();
 
